f386c28302b5e03a2a409fab8e4c2bc017892f1f
Yeonuk Hwang <younuk23@gmail.com>
2023-03-21 16:52:54 +0900

    FEAT: Build TODO Application with TDD process

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
+
+let id = 1;
+
 function App() {
-  return <h1>Hello, World</h1>;
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addNewTodo = () => {
+    setTodos((prev) => [...prev, { id: id++, todo: newTodo }]);
+  };
+
+  const deleteTodo = (deleteId) => {
+    setTodos((prev) => prev.filter(({ id }) => id !== deleteId));
+  };
+
+  return (
+    <section>
+      <h1>Todo List</h1>
+      <input
+        placeholder="type new todo"
+        value={newTodo}
+        onChange={({ target }) => setNewTodo(target.value)}
+      />
+      <button onClick={addNewTodo}>add</button>
+      <ul>
+        {todos.map(({ id, todo }) => (
+          <div>
+            <li key={id}>{todo}</li>
+            <button onClick={() => deleteTodo(id)}>delete</button>
+          </div>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/hello, world/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  it("App rendering", () => {
+    render(<App />);
+
+    const heading = screen.getByRole("heading");
+    const input = screen.getByPlaceholderText("type new todo");
+    const button = screen.getByText("add");
+
+    expect(heading).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  it("type input, button click => todo add", () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText("type new todo");
+    const button = screen.getByText("add");
+
+    userEvent.type(input, "learn tdd");
+    userEvent.click(button);
+
+    expect(screen.getByText(/learn tdd/i)).toBeInTheDocument();
+  });
+
+  it("todo delete", () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText("type new todo");
+    const button = screen.getByText("add");
+
+    userEvent.type(input, "learn tdd");
+    userEvent.click(button);
+
+    const todo = screen.getByText(/learn tdd/i);
+    expect(todo).toBeInTheDocument();
+
+    const deleteBtn = screen.getByText("delete");
+    userEvent.click(deleteBtn);
+
+    expect(todo).not.toBeInTheDocument();
+  });
 });
